f3d8190683559089015461e48a35cc12c71a12ec
Matt Born <mattborn@users.noreply.github.com>
2022-07-15 10:54:48 -0600

    Add routing, map marker

--- bigfoot.js
+++ bigfoot.js
@@ -1,14 +1,24 @@
+const q = document.querySelectorAll.bind(document)
+const g = document.getElementById.bind(document)
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGJvcm4iLCJhIjoiY2w1Ym0wbHZwMDh3eTNlbnh1aW51cm0ydyJ9.Z5h4Vkk8zqjf6JydrOGXGA'
+const state = {}
 
-navigator.geolocation.getCurrentPosition(p => {
-  renderMap([p.coords.longitude, p.coords.latitude])
-}, e => {
-  console.log(e)
-  renderMap([-73.95, 40.69])
-})
+let coords
 
-function renderMap(coords) {
+function initMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGJvcm4iLCJhIjoiY2w1Ym0wbHZwMDh3eTNlbnh1aW51cm0ydyJ9.Z5h4Vkk8zqjf6JydrOGXGA'
+
+  navigator.geolocation.getCurrentPosition(p => {
+    coords = [p.coords.longitude, p.coords.latitude]
+    renderMap()
+  }, e => {
+    coords = [-73.95, 40.69]
+    renderMap()
+  })
+}
+
+function renderMap() {
+  // console.log(coords)
   const map = new mapboxgl.Map({
     center: coords,
     container: 'map',
@@ -16,6 +26,9 @@ function renderMap(coords) {
     zoom: 12
   })
   map.addControl(new mapboxgl.NavigationControl())
+  const marker1 = new mapboxgl.Marker()
+    .setLngLat(coords)
+    .addTo(map)
 }
 
 ScrollReveal().reveal('.card-thing', {
@@ -24,3 +37,44 @@ ScrollReveal().reveal('.card-thing', {
   interval: 100,
   origin: 'bottom',
 })
+
+//mattborn.com/experiment/2044/
+
+function goHere(path) {
+  history.pushState({}, path, location.origin +'/demo/'+ path)
+  render(path)
+}
+
+q('[data-route]').forEach(el => el.addEventListener('click', (e) => {
+  // this no longer works for imported pages
+  e.preventDefault()
+  goHere(el.dataset.route)
+}))
+
+window.addEventListener('popstate', (e) => render(getPath()))
+
+function getPath() {
+  return location.pathname.substring(5) || location.hash.substring(2) || 'search'
+}
+
+function render(path) {
+  // if route has a template, load it
+  // const html = library.getElementById(path)
+  // if (html) document.getElementById('scroll').innerHTML = html.innerHTML
+  q('.content').forEach(el => el.classList.remove('active'))
+  g(path).classList.add('active')
+
+  if (path === 'map') initMap()
+
+  // mark anything pointing to this route as active
+  markActive(path)
+}
+
+function markActive(path) {
+  q('[data-route]').forEach(el => {
+    // remove active class from anything pointing to a route
+    el.classList.remove('active')
+    // add active class to matching routes
+    if (el.dataset.route === path) el.classList.add('active')
+  })
+}
